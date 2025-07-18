eb531f109a7a2503119b57101c15bb2431146454
hemalatanayak <nayakhemalata9@gmail.com>
2025-07-18 00:29:12 -0400

    modified:   Utils/ROOT_to_PKL/run_all_configs.sh

--- Utils/ROOT_to_PKL/run_all_configs.sh
+++ Utils/ROOT_to_PKL/run_all_configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# List of x, y, z combinations
+# Define list of x y z combinations
 configs=(
   "25 25 100"
   "25 25 200"
@@ -10,8 +10,19 @@ configs=(
   "50 50 200"
 )
 
-# Loop and run the main script
+# Loop through each config
 for config in "${configs[@]}"; do
+  echo "=============================="
+  echo "Running for config: $config"
+  echo "------------------------------"
+  
   ./run_pion_custom_start_end.sh 1 100 $config
+  wait  # Waits for the above to finish (redundant here, but safe)
+
   ./run_proton_custom_start_end.sh 1 100 $config
+  wait
+
+  echo "Finished config: $config"
+  echo "=============================="
+  echo ""
 done
